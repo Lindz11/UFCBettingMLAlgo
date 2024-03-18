d9486b90574603a2ca8eda148e75a123ccfb1113
Lindz11 <lindsaygriffith@aol.com>
2024-03-18 04:52:42 -0400

    Adding updates on spacing and formatting

--- Web Scrapping/favoritevsunderdogodds.py
+++ Web Scrapping/favoritevsunderdogodds.py
@@ -21,6 +21,7 @@ def UFC_Odds_Event_Links(source):
 
 ## def UFC_Odds_Fighter_Info(source): 
 
+
 event_info = UFC_Odds_Event_Links(soup)
 for event in event_info: 
     print("Date", event['date'])
--- Web Scrapping/fightevents.py
+++ Web Scrapping/fightevents.py
@@ -69,17 +69,6 @@ def UFC_Fight_Statistics(links):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 links = UFC_Event_Links(soup)
 dates = UFC_Fight_Card_Date(soup)
 fight_links = UFC_Fight_Info(links)
